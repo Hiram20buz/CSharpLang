b2a397af30f518b42d8ca288f4359f49ba62d0e2
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-07-01 14:12:11 -0700

    Create words.cs

---
+++ words.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+            int y =1;
+            while (
+count < 9)
+            {
+              if(words[count].Contains(letter))
+              {
+               Console.WriteLine(words[count]);
+               y += count;
+              }
+              count ++;
+            }
+             if(y == 0 || y == 1)
+             {
+                 Console.WriteLine("No match");
+             }
+            
+        }
+    }
+}
