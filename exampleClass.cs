92c80684cbe2fac540b4a8197353b0f5dd5ee784
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-07-01 12:41:15 -0700

    Create exampleClass.cs

---
+++ exampleClass.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        class Dog
+        {
+            public string name;
+            public int age;
+        }
+
+        static void Main(string[] args)
+        {
+            Dog bob = new Dog();
+            bob.name = "Bobby";
+            bob.age = 3;
+
+            Console.WriteLine(bob.age);
+        }
+    }
+}
